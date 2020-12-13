dad5af00f54f0cae5dea3716c90e921f8ffa8b1f
mninc <manicminer106@gmail.com>
2020-12-13 16:15:56 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# gladiator.tf stats on backpack.tf
+
+A script to put links to gladiator.tf listing snapshots on some backpack.tf pages.
+
+## Installation
+
+1. Install [Greasemonkey (Firefox)](http://www.greasespot.net/) or [Tampermonkey (Chrome)](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo).
+2. Open the [script in raw view](https://github.com/mninc/gladiator.tf-bot-owner-script/raw/master/gladiator.user.js).
+3. Install.
+
+
+## Images
+![Stats page](https://cdn.discordapp.com/attachments/445162468541464576/787714168315707392/unknown.png)
+![Suggestion page](https://cdn.discordapp.com/attachments/445162468541464576/787713469935386642/unknown.png)
+![History page](https://cdn.discordapp.com/attachments/445162468541464576/787713984043155526/unknown.png)
---
+++ gladiatortf-stats-bptf.user.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name            Gladiator.tf stats backpack.tf integration
+// @namespace       https://gladiator.tf
+// @version         1.0
+// @description     Provides a link to Gladiator.tf listing snapshots on backpack.tf pages
+// @author          manic
+// @grant           none
+// @license         MIT
+
+// @homepageURL     https://github.com/mninc/gladiator.tf-stats-bptf
+// @supportURL      https://github.com/mninc/gladiator.tf-stats-bptf/issues
+// @downloadURL     https://github.com/mninc/gladiator.tf-stats-bptf/raw/master/gladiatortf-stats-bptf.user.js
+
+// @run-at          document-end
+// @match           https://backpack.tf/stats*
+// @match           https://backpack.tf/suggestion/*
+// @match           https://backpack.tf/item/*
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    let item = $('.stats-header-title').text();
+    if (!item) item = $('.header .item-name').text();
+    if (!item) item = $('.item-text h2').text();
+
+    item = item.trim().replace("%", "%25");
+
+    $('#classifieds').append(`
+        <a class="btn btn-default" href="https://gladiator.tf/listings?item=${item}" target="_blank"><i class="fa fa-history fa-fw"></i> Gladiator.tf snapshots</a>
+    `);
+    $('.panel:first .panel-extras').append(`
+        <a class="btn btn-panel" href="https://gladiator.tf/listings?item=${item}" target="_blank"><i class="fa fa-history fa-fw"></i> Gladiator.tf snapshots</a>
+    `);
+})();
